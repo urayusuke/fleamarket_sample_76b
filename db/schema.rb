f5fd62f1c1e413981c1454bed83dc71c6ba9da49
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-04 12:24:14 +0900

    Create model(Remove later)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_104855) do
+ActiveRecord::Schema.define(version: 2020_06_04_031525) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
