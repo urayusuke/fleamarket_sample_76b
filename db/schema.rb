d163e0adb692f66ec884b335fc4c4b36bf3e3061
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-04 12:14:55 +0900

    Prepare show view

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_06_03_104855) do
 
-  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
