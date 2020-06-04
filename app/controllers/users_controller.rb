8e66dfc1a5f531c6e623b8ae52cfd9867c022bda
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-04 18:55:20 +0900

    Create user-logout view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def logout
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products
-  resources :users
+  resources :users do
+    member do
+      get "logout"
+    end
+  end
 end
