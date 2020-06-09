f5577e8bde5e9564fb92c570a089cea71765aecc
Odai-SiliconValley <shimashimanomi0422@ezweb.ne.jp>
2020-06-09 18:24:49 +0900

    add-ex-function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: [:index, :new, :edit, :create]
+
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
@@ -19,11 +21,27 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+
   def purchase
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :content, :price, images_attributes: [:src]).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :content, :price, :prefecture_id, :product_status_id, :delively_days_id ,:delively_cost_id, images_attributes: [:src, :_destroy, :id]).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
