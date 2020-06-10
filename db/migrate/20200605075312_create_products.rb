47a57d1ee3ed5b70d40bdba1b19186b764f83bb5
densuke8 <64817260+densuke8@users.noreply.github.com>
2020-06-10 18:12:15 +0900

    user_id削除→seller_id,buyer_idの追加

--- db/migrate/20200605075312_create_products.rb
+++ db/migrate/20200605075312_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.references :user                   ,foreign_key: true,null: false
+      t.references :seller                 ,foreign_key: { to_table: :users } ,null: false  
+      t.references :buyer                  ,foreign_key: { to_table: :users }
 
       t.string :name                       ,null: false
       t.string :content                    ,null: false
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_034845) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "seller_id", null: false
+    t.bigint "buyer_id"
     t.string "name", null: false
     t.string "content", null: false
     t.string "bland_name"
@@ -66,8 +67,9 @@ ActiveRecord::Schema.define(version: 2020_06_09_034845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.index ["buyer_id"], name: "index_products_on_buyer_id"
     t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
+    t.index ["seller_id"], name: "index_products_on_seller_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -92,5 +94,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_034845) do
   add_foreign_key "credits", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "categories"
-  add_foreign_key "products", "users"
+  add_foreign_key "products", "users", column: "buyer_id"
+  add_foreign_key "products", "users", column: "seller_id"
 end
