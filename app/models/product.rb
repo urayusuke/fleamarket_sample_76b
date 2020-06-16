7a446e3bcdd51b5c84f3e76c6ab01a1886563dd0
Eiji Otsuki <bigoonage0104@gmail.com>
2020-06-16 10:20:48 +0900

    Create comment table

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,6 +16,7 @@ class Product < ApplicationRecord
   belongs_to_active_hash :delively_method
 
   has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # validation
   accepts_nested_attributes_for :images, allow_destroy: true
